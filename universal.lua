19fca629a54f85db2843261336ff0a8b398183be
BlitzX Hub <orelmax2207@gmail.com>
2025-10-19 11:17:09 +0200

    Add universal script loading based on game PlaceId

--- universal.lua
+++ universal.lua
@@ -1,1 +1,11 @@
-soon
+local id = game.PlaceId
+local universal = "https://raw.githubusercontent.com/CheaterOnTop/ByYTBmaxxmaxx_11-universalV2/refs/heads/main/universal.lua"
+local Counter_Blox = "https://raw.githubusercontent.com/CheaterOnTop/ByYTBmaxxmaxx_11-universalV2/refs/heads/main/Counter%20Blox%20Beta"
+
+if id == 286090429 then
+    loadstring(game:HttpGet(universal))()
+elseif id == 301549746 then
+    loadstring(game:HttpGet(Counter_Blox))()
+else
+    loadstring(game:HttpGet(universal))()
+end
